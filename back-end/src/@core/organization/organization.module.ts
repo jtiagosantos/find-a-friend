bb243e8210d6f64072ea56887a9498df6d30f1c7
tiagosan040 <tiagosan040@gmail.com>
2024-01-12 00:29:13 -0300

    feat(back-end): adjust prisma as a module

--- back-end/src/@core/organization/organization.module.ts
+++ back-end/src/@core/organization/organization.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { OrganizationController } from './organization.controller';
-import { PrismaService } from 'src/services/database/prisma.service';
 import { RegisterOrganizationService } from './services/register-organization.service';
 import { GetOrganizationService } from './services/get-organization.service';
 import { HashingModule } from 'src/services/hashing/hashing.module';
+import { PrismaModule } from 'src/services/database/prisma.module';
 
 @Module({
   controllers: [OrganizationController],
-  providers: [PrismaService, RegisterOrganizationService, GetOrganizationService],
-  imports: [HashingModule],
+  providers: [RegisterOrganizationService, GetOrganizationService],
+  imports: [HashingModule, PrismaModule],
 })
 export class OrganizationModule {}
