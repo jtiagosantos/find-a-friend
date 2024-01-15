3d1489eee2a1c64165ee2d3b65db1fa67aceb08c
tiagosan040 <tiagosan040@gmail.com>
2024-01-15 10:25:56 -0300

    feat(back-end): add validation for an organization cannot delete a pet from other organization

--- back-end/src/@core/pet/pet.controller.ts
+++ back-end/src/@core/pet/pet.controller.ts
@@ -19,6 +19,7 @@ import { Organization } from '../organization/decorators/organization.decorator'
 import { OrganizationData } from '../organization/types/organization-data.type';
 import { AuthGuard } from 'src/services/auth/auth.guard';
 import { PetNotFoundException } from './exceptions/pet-not-found.exception';
+import { PermissionDeniedException } from './exceptions/permission-denied.exception';
 
 @Controller('pets')
 export class PetController {
@@ -92,14 +93,19 @@ export class PetController {
 
   @UseGuards(AuthGuard)
   @Delete(':id')
-  public async delete(@Param('id') id: string) {
-    const PetExists = await this.getPetService.execute({ id });
+  public async delete(
+    @Param('id') id: string,
+    @Organization() organization: OrganizationData,
+  ) {
+    const pet = await this.getPetService.execute({ id });
 
-    if (!PetExists) {
+    if (!pet) {
       throw new PetNotFoundException();
     }
 
-    //TODO: validate if the give pet id is from the authenticated organization
+    if (pet.organizationId !== organization.id) {
+      throw new PermissionDeniedException();
+    }
 
     await this.deletePetService.execute({ id });
   }
