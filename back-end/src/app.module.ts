edc17e0566614b8bdadea41567c5ab1d3df6c264
tiagosan040 <tiagosan040@gmail.com>
2024-01-15 22:23:04 -0300

    feat(back-end): register all modules in app module

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { OrganizationModule } from './@core/organization/organization.module';
+import { PetModule } from './@core/pet/pet.module';
+import { PhotoModule } from './@core/photo/photo.module';
+import { RequirementForAdoptionModule } from './@core/requirement-for-adoption/requirement-for-adoption.module';
 
 @Module({
-  imports: [],
+  imports: [OrganizationModule, PetModule, PhotoModule, RequirementForAdoptionModule],
   controllers: [],
   providers: [],
 })
