354ed47821971e21d4cb54f8c617951883f547eb
tiagosan040 <tiagosan040@gmail.com>
2024-01-18 17:50:19 -0300

    fix(back-end): adjust validations in update pet photos DTO

--- back-end/src/@core/photo/dtos/update-pet-photos.dto.ts
+++ back-end/src/@core/photo/dtos/update-pet-photos.dto.ts
@@ -1,8 +1,12 @@
-import { ArrayNotEmpty, IsArray, IsString } from 'class-validator';
+import { ArrayNotEmpty, IsArray, IsString, IsNotEmpty } from 'class-validator';
 
 export class UpdatePetPhotosDTO {
   @IsArray()
   @ArrayNotEmpty({ message: 'photos must be at least one element' })
   @IsString({ each: true, message: 'Each element of the array must be a string' })
+  @IsNotEmpty({
+    each: true,
+    message: 'Each element of the array must be not an empty string',
+  })
   photos: Array<string>;
 }
