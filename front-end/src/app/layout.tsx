d2d541075013d68a2d1f894a58c77b798cbd5a8e
tiagosan040 <tiagosan040@gmail.com>
2024-01-22 20:07:07 -0300

    style(front-end): add custom font

--- front-end/src/app/layout.tsx
+++ front-end/src/app/layout.tsx
@@ -1,4 +1,7 @@
 import './globals.css';
+import { Nunito } from 'next/font/google';
+
+const nunito = Nunito({ subsets: ['latin'] });
 
 export default function RootLayout({
   children,
@@ -7,7 +10,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body>{children}</body>
+      <body className={nunito.className}>{children}</body>
     </html>
   );
 }
