67d2c7f05d4b1e4cf8c48f0531db9eee0cb887f2
tiagosan040 <tiagosan040@gmail.com>
2024-01-25 15:18:51 -0300

    feat(back-end): configure cors on back-end

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -4,8 +4,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+  });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(3333);
+
+  await app.listen(4000);
 }
 
 bootstrap();
