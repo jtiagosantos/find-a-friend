f5ed0879b5c07ac8f832169df3d539627cebc1fd
tiagosan040 <tiagosan040@gmail.com>
2024-01-25 22:58:42 -0300

    feat(front-end): add loading on button and set style for disable state

--- front-end/src/shared/components/button.tsx
+++ front-end/src/shared/components/button.tsx
@@ -1,19 +1,49 @@
 import { twMerge } from 'tailwind-merge';
+import { ThreeDots } from 'react-loader-spinner';
 import type { FC, ComponentProps } from 'react';
 
-export const Button: FC<ComponentProps<'button'>> = ({
+type ButtonProps = ComponentProps<'button'> & {
+  isLoading?: boolean;
+};
+
+export const Button: FC<ButtonProps> = ({
   className,
   children,
+  isLoading = false,
   ...props
 }) => {
   return (
     <button
+      disabled={isLoading}
       className={twMerge(
-        'bg-[#0D3B66] w-full rounded-[20px] py-5 text-[#ffffff] text-[20px] font-extrabold',
+        `
+          bg-[#0D3B66] 
+          w-full 
+          rounded-[20px] 
+          py-5 
+          text-[#ffffff] 
+          text-[20px] 
+          font-extrabold 
+          flex 
+          items-center 
+          justify-center 
+          disabled:opacity-60
+        `,
         className,
       )}
       {...props}>
-      {children}
+      {isLoading ? (
+        <ThreeDots
+          height="30"
+          width="40"
+          radius="9"
+          color="#fff"
+          ariaLabel="three-dots-loading"
+          visible={true}
+        />
+      ) : (
+        <>{children}</>
+      )}
     </button>
   );
 };
